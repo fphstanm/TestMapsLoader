6a6e74796309da6077ed86ffbeb0927c3903912c
Philip <fphstanm@gmail.com>
2019-11-04 16:55:29 +0200

    add Presenter.swift and moved funcs to it

--- TestMapsLoader/Controller/CountriesViewController.swift
+++ TestMapsLoader/Controller/CountriesViewController.swift
@@ -19,12 +19,9 @@ class CountriesTableViewController: UIViewController,
     @IBOutlet weak var countriesTableView: UITableView!
     @IBOutlet weak var freeSpace: UILabel!
         
-    var regions: [Region] = []
-//    var documentsURL = URL(string: "http://download.osmand.net/download.php?standard=yes&file=Germany_berlin_europe_2.obf.zip")!
+ 
+    lazy var presenter = CountriesPresenterImp(view: self)
 
-    var mapFile = Data()
-    var downloadedFileUrl: URL?
-    var relocatedFileUrl: URL?
 
     override func viewWillAppear
         (_ animated: Bool) {
@@ -43,93 +40,15 @@ class CountriesTableViewController: UIViewController,
         //WTF?
         navigationController?.navigationBar.barTintColor = UIColor(hex: "#ff8800")
 
-        parseRegionsXML()
-        downloadMap()
+        presenter.parseRegionsXML()
+        presenter.downloadMap()
     }
     
     func setupCountryCell(cell: countryTableViewCell, country: String) {
         cell.setup(country: country)
     }
-    
-    func downloadMap() {
-//        AF.download("https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf")
-//        AF.download("http://download.osmand.net/download.php?standard=yes&file=Germany_berlin_europe_2.obf.zip")
-//        AF.download("https://httpbin.org/image/png") //, to: destination
-        
-        let destination: DownloadRequest.Destination = { _, _ in
-            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            let fileURL = documentsURL.appendingPathComponent("image.png")
-            return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
-        }
-        
-        AF.download("https://httpbin.org/image/png", to: destination)
-        .downloadProgress { progress in
-            print("Download Progress: \(progress.fractionCompleted)")
-        }
-        .responseData { response in
-            if let data = response.value {
-                var file = MapFile()
-                file.name = "regionName"
-                file.archive = data
-                self.downloadedFileUrl = response.fileURL
-            }
-        }
-    }
-    
-    func getMemoryInfo() {
-        let fileURL = URL(fileURLWithPath: NSHomeDirectory() as String)
-        do {
-            let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
-            if let capacity = values.volumeAvailableCapacityForImportantUsage {
-                print("Available capacity for important usage: \(capacity)")
-            } else {
-                print("Capacity is unavailable")
-            }
-        } catch {
-            print("Error retrieving capacity: \(error.localizedDescription)")
-        }
-    }
-    
-    func parseRegionsXML() {
-        let xmlString = fileToString(name: "CountriesInfo", fileType: "xml")
-        let xml = try! XML.parse(xmlString)
-        let regionsList = xml.regions_list[0].region
-        
-        //TODO make it easier
-        regionsList.forEach { continentInfo in
-            let continentName = continentInfo.first.attributes["name"]
-            let continent = Region(name: continentName!, regions: [])
-            regions.append(continent)
-            let continentIndex = regions.count - 1
-            
-            continentInfo.region.forEach { countryInfo in
-                let countryName = countryInfo.first.attributes["name"]
-                let country = Region(name: countryName!, regions: []) //TODO mb nil?
-                regions[continentIndex].regions?.append(country)
-                let countryIndex = regions.count - 1
-                
-                countryInfo.region.forEach { region in
-                    let regionName = region.first.attributes["name"]
-                    let area = Region(name: regionName!, regions: nil) //TODO force unwrap
-                    regions[continentIndex].regions?[countryIndex].regions?.append(area)
-//                    print(countryName!, " ", regionName!)
-                }
-            }
-        }
-    }
 
-    func fileToString(name: String, fileType: String) -> String {
-        var xmlString = ""
-        if let path = Bundle.main.path(forResource: name, ofType: fileType) {
-            do {
-                xmlString = try String(contentsOfFile: path)
-            } catch {
-                // contents could not be loaded
-            }
-        }
-        
-        return xmlString
-    }
+
 }
 
 extension CountriesTableViewController {
@@ -139,14 +58,14 @@ extension CountriesTableViewController {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let countriesQuantity = regions[0].regions?.count else { return 0 }
+        guard let countriesQuantity = presenter.regions[0].regions?.count else { return 0 }
         
         return countriesQuantity
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = countriesTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! countryTableViewCell
-        cell.setup(country: regions[0].regions![indexPath.row].name) //TODO force unwrap
+        cell.setup(country: presenter.regions[0].regions![indexPath.row].name) //TODO force unwrap
         
         return cell
     }
@@ -159,7 +78,6 @@ extension CountriesTableViewController {
 //        imageDetailController.imageName = mainDataArray[indexPath.row].name!
 //        imageDetailController.imageDescription = mainDataArray[indexPath.row].description!
 //        
-//        self.navigationController!.pushViewController(imageDetailController, animated: true)
         let countryRegionsViewController = self.storyboard!.instantiateViewController(withIdentifier: "CountryRegions") as! CountryRegionsViewController
         
         self.navigationController!.pushViewController(countryRegionsViewController, animated: true)
@@ -167,36 +85,6 @@ extension CountriesTableViewController {
     
 }
 
-
-extension Data {
-    func sizeString(units: ByteCountFormatter.Units = [.useAll], countStyle: ByteCountFormatter.CountStyle = .file) -> String {
-        let bcf = ByteCountFormatter()
-        bcf.allowedUnits = units
-        bcf.countStyle = .file
-
-        return bcf.string(fromByteCount: Int64(count))
-    }
-    
-}
-
-extension CountriesTableViewController:  URLSessionDownloadDelegate {
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("downloadLocation:", location)
-        // create destination URL with the original pdf name
-        guard let url = downloadTask.originalRequest?.url else { return }
-        let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        let destinationURL = documentsPath.appendingPathComponent(url.lastPathComponent)
-        // delete original copy
-        try? FileManager.default.removeItem(at: destinationURL)
-        // copy from temp to Document
-        do {
-            try FileManager.default.copyItem(at: location, to: destinationURL)
-            self.downloadedFileUrl = destinationURL
-        } catch let error {
-            print("Copy Error: \(error.localizedDescription)")
-        }
-    }
-}
 
 extension UIColor {
     public convenience init?(hex: String) {
@@ -226,4 +114,4 @@ extension UIColor {
     }
 }
 
-    //                let result = self.realm.objects(MapFile.self).filter("name = 'regionName'")
+//                let result = self.realm.objects(MapFile.self).filter("name = 'regionName'")
---
+++ TestMapsLoader/Presenter/CountriesPresenter.swift
@@ -0,0 +1,129 @@
+//
+//  CountriesPresenter.swift
+//  TestMapsLoader
+//
+//  Created by Philip on 29.10.2019.
+//  Copyright © 2019 Philip. All rights reserved.
+//
+
+
+
+
+import Foundation
+import SwiftyXMLParser
+import Alamofire
+
+protocol CountriesPresenter {
+    
+//    func setupCountryCell(cell: countryTableViewCell, country: String)
+}
+
+
+class CountriesPresenterImp: CountriesPresenter {
+    
+    let view: CountriesTableViewController
+    
+    var downloadedFileUrl: URL?
+    var regions: [Region] = []
+    var mapFile = Data()
+    var relocatedFileUrl: URL?
+
+    
+    init(view: CountriesTableViewController) {
+        self.view = view
+    }
+    
+    func downloadMap() {
+//        AF.download("https://www.hq.nasa.gov/alsj/a17/A17_FlightPlan.pdf")
+//        AF.download("http://download.osmand.net/download.php?standard=yes&file=Germany_berlin_europe_2.obf.zip")
+//        AF.download("https://httpbin.org/image/png") //, to: destination
+        
+        let destination: DownloadRequest.Destination = { _, _ in
+            let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let fileURL = documentsURL.appendingPathComponent("image.png")
+            return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+        }
+        
+        AF.download("https://httpbin.org/image/png", to: destination)
+        .downloadProgress { progress in
+            print("Download Progress: \(progress.fractionCompleted)")
+        }
+        .responseData { response in
+            if let data = response.value {
+                var file = MapFile()
+                file.name = "regionName"
+                file.archive = data
+                self.downloadedFileUrl = response.fileURL
+            }
+        }
+    }
+    
+    func getMemoryInfo() {
+        let fileURL = URL(fileURLWithPath: NSHomeDirectory() as String)
+        do {
+            let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
+            if let capacity = values.volumeAvailableCapacityForImportantUsage {
+                print("Available capacity for important usage: \(capacity)")
+            } else {
+                print("Capacity is unavailable")
+            }
+        } catch {
+            print("Error retrieving capacity: \(error.localizedDescription)")
+        }
+    }
+    
+    func parseRegionsXML() {
+        let xmlString = fileToString(name: "CountriesInfo", fileType: "xml")
+        let xml = try! XML.parse(xmlString)
+        let regionsList = xml.regions_list[0].region
+        
+        //TODO make it easier
+        regionsList.forEach { continentInfo in
+            let continentName = continentInfo.first.attributes["name"]
+            let continent = Region(name: continentName!, regions: [])
+            regions.append(continent)
+            let continentIndex = regions.count - 1
+            
+            continentInfo.region.forEach { countryInfo in
+                let countryName = countryInfo.first.attributes["name"]
+                let country = Region(name: countryName!, regions: []) //TODO mb nil?
+                regions[continentIndex].regions?.append(country)
+                let countryIndex = regions.count - 1
+                
+                countryInfo.region.forEach { region in
+                    let regionName = region.first.attributes["name"]
+                    let area = Region(name: regionName!, regions: nil) //TODO force unwrap
+                    regions[continentIndex].regions?[countryIndex].regions?.append(area)
+//                    print(countryName!, " ", regionName!)
+                }
+            }
+        }
+    }
+    
+    func fileToString(name: String, fileType: String) -> String {
+        var xmlString = ""
+        if let path = Bundle.main.path(forResource: name, ofType: fileType) {
+            do {
+                xmlString = try String(contentsOfFile: path)
+            } catch {
+                // contents could not be loaded
+            }
+        }
+        
+        return xmlString
+    }
+}
+
+ 
+ 
+
+extension Data {
+    func sizeString(units: ByteCountFormatter.Units = [.useAll], countStyle: ByteCountFormatter.CountStyle = .file) -> String {
+        let bcf = ByteCountFormatter()
+        bcf.allowedUnits = units
+        bcf.countStyle = .file
+
+        return bcf.string(fromByteCount: Int64(count))
+    }
+    
+}
